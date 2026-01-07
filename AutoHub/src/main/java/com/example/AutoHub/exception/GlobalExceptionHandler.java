c89505bd2ae2373dbf1d7d7101fada3deb3fc9df
RahulRB-05 <mailrahulrb@gmail.com>
2026-01-07 15:37:07 +0530

    Response Entity Created

--- AutoHub/src/main/java/com/example/AutoHub/exception/GlobalExceptionHandler.java
+++ AutoHub/src/main/java/com/example/AutoHub/exception/GlobalExceptionHandler.java
@@ -9,4 +9,13 @@ import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErrorResponse> notFound(NotFoundException e){
+        ErrorResponse error=new ErrorResponse();
+        error.setMessage(e.getMessage());
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setTimeStamp(LocalDateTime.now());
+
+        return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
+    }
 }
